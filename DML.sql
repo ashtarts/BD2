85227e64275bc1aa60f51a7d3ed0f7c952615165
Ana Lira <101262493+ashtarts@users.noreply.github.com>
2024-04-29 15:52:14 -0300

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,17 +1,19 @@
-INSERT INTO Usuario (Nome, Login, Cel, CPF, End, Genero, ID_Usuario, E_mail, Status_Usuario, Senha_Usuario) 
-VALUES ('João Silva', 'joao123', 987654321, 12345678901, 'Rua A', 'Masculino', 1, 'joao@email.com', 1, 'senha123');
+INSERT INTO E_book (Editora, Autor, Edicao, ID_book, Status, Genero, Area) 
+VALUES ('Editora Y', 'Autor 2', '2nd Edition', 1, 1, 'Ficção', 'Aventura');
 
 
-INSERT INTO Bibliotecario (End, Login, E_mail, Cel, Funcao, ID_Func, Nome, Senha_Func) 
-VALUES ('Rua C', 'bibliotecario1', 'bibliotecario1@email.com', 987654322, 'Atendente', 1, 'José', 'senha456');
+INSERT INTO Devolucao (id, idLivro, quantidade, IdUsuario, datadevolução) 
+VALUES (1, 1, 1, 1, '2024-04-23');
+
+INSERT INTO Espaco (local, disponibilidade, id-evento) 
+VALUES ('Sala de Aula 101', 30, NULL);
+
+INSERT INTO Aluno (data-nascimento, endereço, mesnalidade-id, aluno-id, serie, telefone, email, cpf) 
+VALUES ('2000-05-15', 'Rua D, 123', NULL, '2024001', NULL, '987654321', 'maria@email.com', NULL);
 
 
-INSERT INTO Livro (Autor, Ano, Edicao, Qtd_Exemplares, Titulo, Editora, Status, ID_Livro, Area, Genero) 
-VALUES ('Autor 1', 2000, '1st Edition', 10, 'Livro A', 'Editora X', 1, 1, 'Ficção', 'Aventura');
-
-
-INSERT INTO E_book (Editora, Autor, Edicao, ID_book, Status, Link, Genero, Area) 
-VALUES ('Editora Y', 'Autor 2', '2nd Edition', 1, 1, 'http://example.com/ebook1', 'Ficção', 'Aventura');
+INSERT INTO Professor (prof-id, email, data-nascimento, telefone, salario) 
+VALUES ('P2024', 'carlos@email.com', '1980-02-10', '999888777', NULL);
 
 
 INSERT INTO Devolucao (ID_Devolucao, ID_Livro, Quant_Exemplares, Data_Dev, ID_Func, ID_Usuario) 
@@ -33,4 +35,4 @@ VALUES ('Sala de Aula 101', 30, 'Sala de Aula', 'Bloco A, 1º Andar');
 
 
 INSERT INTO Mensalidade (ID_Aluno, Valor, Data_Vencimento, Status_Pagamento)
-VALUES (1, 500.00, '30/04/2024', 'Pendente');
+VALUES (1, 500.00, '2024-04-30', 'Pendente');
